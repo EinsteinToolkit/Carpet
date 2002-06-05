492373459b55a4511410ba3540474963ec44fe58
schnetter <>
2002-06-05 17:52:00 +0000

    Changed output mode specifiers.
    darcs-hash:20020605175241-07bb3-072ff724b0cbc5ecad261a17f8e0b60cc69ba60e.gz

--- Carpet/CarpetIOASCII/src/ioascii.cc
+++ Carpet/CarpetIOASCII/src/ioascii.cc
@@ -28,7 +28,7 @@
 
 #include "ioascii.hh"
 
-static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/CarpetIOASCII/src/ioascii.cc,v 1.31 2002/04/29 11:27:56 schnetter Exp $";
+static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/CarpetIOASCII/src/ioascii.cc,v 1.32 2002/06/05 19:52:41 schnetter Exp $";
 
 CCTK_FILEVERSION(CarpetIOASCII_ioascii_cc)
 
@@ -272,7 +272,7 @@ int CarpetIOASCII<outdim>
 	    }
 	  }
 	  if (! file.is_open()) {
-	    file.open (filename, ios::app);
+	    file.open (filename, ios::out | ios::ate);
 	    assert (file.good());
 	  }
 	  file << setprecision(15);
