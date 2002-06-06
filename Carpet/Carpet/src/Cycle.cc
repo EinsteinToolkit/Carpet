911156a7bbc76aa1166c91d1f5d123e70640d211
schnetter <>
2002-06-06 19:04:00 +0000

    Flip all timelevels instead of only the current one.
    Flip all timelevels instead of only the current one.
    Add comments about what is left to do wrt. arrays when initialising 3 time
    levels.
    
    darcs-hash:20020606190456-07bb3-8bb306a0be588bcd19fe79ec5f46793210b58c29.gz

--- Carpet/Carpet/src/Cycle.cc
+++ Carpet/Carpet/src/Cycle.cc
@@ -8,7 +8,7 @@
 
 #include "carpet.hh"
 
-static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/Cycle.cc,v 1.7 2002/06/06 14:20:15 schnetter Exp $";
+static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/Cycle.cc,v 1.8 2002/06/06 21:04:56 schnetter Exp $";
 
 CCTK_FILEVERSION(Carpet_Cycle_cc)
 
@@ -47,14 +47,16 @@ namespace Carpet {
     Checkpoint ("%*sFlipTimeLevels", 2*reflevel, "");
     
     for (int group=0; group<CCTK_NumGroups(); ++group) {
-      if (reflevel<arrdata[group].hh->reflevels()
+      if (CCTK_GroupTypeI(group) == CCTK_GF
 	  && CCTK_QueryGroupStorageI(cgh, group)) {
 	for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
 	  
 	  assert (group<(int)arrdata.size());
 	  assert (var<(int)arrdata[group].data.size());
-	  for (int c=0; c<arrdata[group].hh->components(reflevel); ++c) {
-	    arrdata[group].data[var]->cycle (reflevel, c, mglevel);
+	  for (int rl=0; rl<arrdata[group].hh->reflevels(); ++rl) {
+	    for (int c=0; c<arrdata[group].hh->components(rl); ++c) {
+	      arrdata[group].data[var]->flip (rl, c, mglevel);
+	    }
 	  }
 	  
 	}
