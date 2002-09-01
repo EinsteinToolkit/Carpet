d97a84978a85d65528810567536bab36f23e2d7e
schnetter <>
2002-09-01 12:37:00 +0000

    Indentation change.
    darcs-hash:20020901123745-07bb3-df0800adfec335b1f0e60443a0068736e174bf0d.gz

--- Carpet/CarpetIOASCII/src/ioascii.h
+++ Carpet/CarpetIOASCII/src/ioascii.h
@@ -1,4 +1,4 @@
-/* $Header: /home/eschnett/C/carpet/Carpet/Carpet/CarpetIOASCII/src/ioascii.h,v 1.2 2002/09/01 14:37:13 schnetter Exp $ */
+/* $Header: /home/eschnett/C/carpet/Carpet/Carpet/CarpetIOASCII/src/ioascii.h,v 1.3 2002/09/01 14:37:45 schnetter Exp $ */
 
 #ifndef CARPETIOASCII_H
 #define CARPETIOASCII_H
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-int CarpetIOASCIIStartup (void);
+  int CarpetIOASCIIStartup (void);
   
 #ifdef __cplusplus
 } /* extern "C" */
