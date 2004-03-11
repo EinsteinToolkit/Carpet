ad728f0c490ccb5f1425f39fe15651f3560b71ff
cott <>
2004-03-11 09:45:00 +0000

    Made function call in meta mode really work...
    darcs-hash:20040311094555-19929-decceccd77b970a00743c8e45f089967a6f2c7d5.gz

--- Carpet/Carpet/src/CallFunction.cc
+++ Carpet/Carpet/src/CallFunction.cc
@@ -11,7 +11,7 @@
 #include "carpet.hh"
   
 extern "C" {
-  static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/CallFunction.cc,v 1.15 2004/03/11 10:16:50 cott Exp $";
+  static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/CallFunction.cc,v 1.16 2004/03/11 10:45:55 cott Exp $";
   CCTK_FILEVERSION(Carpet_Carpet_CallFunction_cc);
 }
 
@@ -31,13 +31,10 @@ namespace Carpet {
     
     cGH* cgh = (cGH*)data;
     
-// TODO: disable temporarily
-//    if (attribute->meta || is_meta_mode()) {
-    if (is_meta_mode()) {
+    if (attribute->meta || is_meta_mode()) {
       // Convtest operation
       
       if (do_meta_mode) {
-        assert (is_meta_mode());
         Checkpoint ("Meta mode call at %s to %s::%s",
                     attribute->where, attribute->thorn, attribute->routine);
 	BEGIN_META_MODE(cgh) {
