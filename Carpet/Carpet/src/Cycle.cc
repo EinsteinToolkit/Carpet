a4698a7cca617c468d9fc65c9f919ffaba1a35cc
schnetter <>
2004-04-22 09:37:00 +0000

    Allow cycling grid variables with 2 time levels.
    darcs-hash:20040422093722-07bb3-5ecb3344ca5fe2ac7b4eca38fdb7288e6d50a72b.gz

--- Carpet/Carpet/src/Cycle.cc
+++ Carpet/Carpet/src/Cycle.cc
@@ -9,7 +9,7 @@
 #include "carpet.hh"
 
 extern "C" {
-  static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/Cycle.cc,v 1.17 2004/03/23 19:30:14 schnetter Exp $";
+  static const char* rcsid = "$Header: /home/eschnett/C/carpet/Carpet/Carpet/Carpet/src/Cycle.cc,v 1.18 2004/04/22 11:37:22 schnetter Exp $";
   CCTK_FILEVERSION(Carpet_Carpet_Cycle_cc);
 }
 
@@ -77,47 +77,35 @@ namespace Carpet {
         if (num_vars>0) {
           const int var0 = CCTK_FirstVarIndexI(group);
           assert (var0>=0);
-          const int num_tl = CCTK_NumTimeLevelsFromVarI(var0);
-          switch (num_tl) {
-          case 1:
-            // Do nothing
+          
+          switch (CCTK_GroupTypeI(group)) {
+            
+          case CCTK_GF:
+            for (int m=0; m<(int)arrdata.at(group).size(); ++m) {
+              for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
+                assert (var<(int)arrdata.at(group).at(m).data.size());
+                for (int c=0; c<arrdata.at(group).at(m).hh->components(reflevel); ++c) {
+                  arrdata.at(group).at(m).data.at(var)->flip (reflevel, c, mglevel);
+                }
+              }
+            }
             break;
-          case 3:
-            // Flip
-            switch (CCTK_GroupTypeI(group)) {
-              
-            case CCTK_GF:
-              for (int m=0; m<(int)arrdata.at(group).size(); ++m) {
-                for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-                  assert (var<(int)arrdata.at(group).at(m).data.size());
-                  for (int c=0; c<arrdata.at(group).at(m).hh->components(reflevel); ++c) {
-                    arrdata.at(group).at(m).data.at(var)->flip (reflevel, c, mglevel);
-                  }
+            
+          case CCTK_SCALAR:
+          case CCTK_ARRAY:
+            if (do_global_mode) {
+              for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
+                assert (var<(int)arrdata.at(group).at(0).data.size());
+                for (int c=0; c<arrdata.at(group).at(0).hh->components(0); ++c) {
+                  arrdata.at(group).at(0).data.at(var)->flip (0, c, mglevel);
                 }
               }
-              break;
-              
-            case CCTK_SCALAR:
-            case CCTK_ARRAY:
-              if (do_global_mode) {
-                for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-                  assert (var<(int)arrdata.at(group).at(0).data.size());
-                  for (int c=0; c<arrdata.at(group).at(0).hh->components(0); ++c) {
-                    arrdata.at(group).at(0).data.at(var)->flip (0, c, mglevel);
-                  }
-                }
-              }
-              break;
-              
-            default:
-              assert (0);
-            } // switch grouptype
+            }
             break;
             
           default:
-            // Error
             assert (0);
-          } // switch num_tl
+          } // switch grouptype
           
         } // if num_vars>0
       } // if storage
