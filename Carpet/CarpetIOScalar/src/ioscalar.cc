3c1aa5845392d4b6ff50b667981e195c18464f42
Erik Schnetter <schnetter@aei.mpg.de>
2005-06-17 20:12:00 +0000

    CarpetIOScalar: Handle IO::out_fileinfo="axis labels"
    darcs-hash:20050617201217-891bb-e59a6b5330b66d13b12cd61e30f4bc7899d8e4bd.gz

--- Carpet/CarpetIOScalar/src/ioscalar.cc
+++ Carpet/CarpetIOScalar/src/ioscalar.cc
@@ -279,6 +279,8 @@ namespace CarpetIOScalar {
               bool want_other = false;
               if (CCTK_EQUALS (out_fileinfo, "none")) {
                 // do nothing
+              } else if (CCTK_EQUALS (out_fileinfo, "axis labels")) {
+                // do nothing
               } else if (CCTK_EQUALS (out_fileinfo, "creation date")) {
                 want_date = true;
               } else if (CCTK_EQUALS (out_fileinfo, "parameter filename")) {
