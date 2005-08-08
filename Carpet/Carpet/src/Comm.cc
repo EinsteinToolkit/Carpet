20491415a0f46d48ba0471bf9aeefe59668f59a9
Erik Schnetter <schnetter@aei.mpg.de>
2005-08-08 19:47:00 +0000

    Carpet: Handle negative time steps correctly during prolongation
    darcs-hash:20050808194745-891bb-74c3f74d1b5a5886ea63e11671aa7f425d5a8daa.gz

--- Carpet/Carpet/src/Comm.cc
+++ Carpet/Carpet/src/Comm.cc
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cmath>
 #include <cstdlib>
 
 #include "cctk.h"
@@ -138,7 +139,8 @@ namespace Carpet {
     const int tl = 0;
 
     // use the current time here (which may be modified by the user)
-    const CCTK_REAL time = (cctkGH->cctk_time - initial_time) / delta_time;
+    const CCTK_REAL time
+      = (cctkGH->cctk_time - initial_time) / abs(delta_time);
 
     for (comm_state state(groups.vartype); ! state.done(); state.step()) {
       for (int group = 0; group < groups.members.size(); ++group) {
