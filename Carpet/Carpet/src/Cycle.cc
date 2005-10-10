c8ab9f9602c85056def7fe3730cad3089a462288
Erik Schnetter <schnetter@aei.mpg.de>
2005-10-10 21:02:00 +0000

    Carpet: Small cleanup in Cycle.cc
    Remove some superfluous assert statements.
    
    darcs-hash:20051010210259-891bb-57c7fb73ea780382b562ac5cbd41f1c5b41d6c8a.gz

--- Carpet/Carpet/src/Cycle.cc
+++ Carpet/Carpet/src/Cycle.cc
@@ -22,16 +22,13 @@ namespace Carpet {
     assert (is_level_mode());
     
     for (int group=0; group<CCTK_NumGroups(); ++group) {
-      assert (group<(int)arrdata.size());
       if (CCTK_QueryGroupStorageI(cgh, group)) {
         switch (CCTK_GroupTypeI(group)) {
           
         case CCTK_GF:
           assert (reflevel>=0 and reflevel<reflevels);
-          for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-            for (int m=0; m<(int)arrdata.at(group).size(); ++m) {
-              assert (m<(int)arrdata.at(group).size());
-              assert (var<(int)arrdata.at(group).at(m).data.size());
+	  for (int m=0; m<(int)arrdata.at(group).size(); ++m) {
+	    for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
               for (int c=0; c<arrdata.at(group).at(m).hh->components(reflevel); ++c) {
                 arrdata.at(group).at(m).data.at(var)->cycle (reflevel, c, mglevel);
               }
@@ -43,7 +40,6 @@ namespace Carpet {
         case CCTK_ARRAY:
           if (do_global_mode) {
             for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-              assert (var<(int)arrdata.at(group).at(0).data.size());
               for (int c=0; c<arrdata.at(group).at(0).hh->components(0); ++c) {
                 arrdata.at(group).at(0).data.at(var)->cycle (0, c, mglevel);
               }
@@ -66,7 +62,6 @@ namespace Carpet {
     assert (is_level_mode());
     
     for (int group=0; group<CCTK_NumGroups(); ++group) {
-      assert (group<(int)arrdata.size());
       if (CCTK_QueryGroupStorageI(cgh, group)) {
         const int num_vars = CCTK_NumVarsInGroupI(group);
         if (num_vars>0) {
@@ -78,7 +73,6 @@ namespace Carpet {
           case CCTK_GF:
             for (int m=0; m<(int)arrdata.at(group).size(); ++m) {
               for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-                assert (var<(int)arrdata.at(group).at(m).data.size());
                 for (int c=0; c<arrdata.at(group).at(m).hh->components(reflevel); ++c) {
                   arrdata.at(group).at(m).data.at(var)->flip (reflevel, c, mglevel);
                 }
@@ -90,7 +84,6 @@ namespace Carpet {
           case CCTK_ARRAY:
             if (do_global_mode) {
               for (int var=0; var<CCTK_NumVarsInGroupI(group); ++var) {
-                assert (var<(int)arrdata.at(group).at(0).data.size());
                 for (int c=0; c<arrdata.at(group).at(0).hh->components(0); ++c) {
                   arrdata.at(group).at(0).data.at(var)->flip (0, c, mglevel);
                 }
