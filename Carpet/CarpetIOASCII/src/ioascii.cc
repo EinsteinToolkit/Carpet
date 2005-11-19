8a311ef0b05b0508c3b36e953ee3543a84890ebf
Erik Schnetter <schnetter@cct.lsu.edu>
2005-11-19 21:23:00 +0000

    CarpetIOASCII: Put the unique build ID into the output files
    darcs-hash:20051119212349-dae7b-4a59559f65890620631c265f391a38b0934e210a.gz

--- Carpet/CarpetIOASCII/src/ioascii.cc
+++ Carpet/CarpetIOASCII/src/ioascii.cc
@@ -685,19 +685,35 @@ namespace CarpetIOASCII {
 #else
                   char const * const run_user = getenv ("USER");
 #endif
-                  char run_date [1000];
-                  Util_CurrentDate (sizeof run_date, run_date);
-                  char run_time [1000];
-                  Util_CurrentTime (sizeof run_time, run_time);
-                  file << "# created on " << run_host
-                       << " by " << run_user
-                       << " on " << run_date
-                       << " at " << run_time << endl;
-
-                  if (want_parfilename) {
-                    char parameter_filename [10000];
-                    CCTK_ParameterFilename (sizeof parameter_filename, parameter_filename);
-                    file << "# parameter filename: \"" << parameter_filename << "\"" << endl;
+                      char run_date [1000];
+                      Util_CurrentDate (sizeof run_date, run_date);
+                      char run_time [1000];
+                      Util_CurrentTime (sizeof run_time, run_time);
+                      file << "# created on " << run_host
+                           << " by " << run_user
+                           << " on " << run_date
+                           << " at " << run_time << endl;
+                    }
+                    if (want_parfilename) {
+                      char parameter_filename [10000];
+                      CCTK_ParameterFilename (sizeof parameter_filename, parameter_filename);
+                      file << "# parameter filename: \"" << parameter_filename << "\"" << endl;
+                    }
+                    if (want_other) {
+                      if (CCTK_IsFunctionAliased ("UniqueSimulationID")) {
+                        char const * const job_id
+                          = (char const *) UniqueSimulationID (cctkGH);
+                        file << "# Simulation ID: " << job_id << endl;
+                      }
+                    }
+                    file << "#" << endl;
+                  }
+                  if (one_file_per_group) {
+                    char* groupname = CCTK_GroupNameFromVarI(n);
+                    file << "# " << groupname;
+                    free (groupname);
+                  } else {
+                    file << "# " << varname;
                   }
                   if (want_other) {
                     if (CCTK_IsFunctionAliased ("UniqueSimulationID")) {
