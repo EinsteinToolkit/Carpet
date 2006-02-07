3be3704f4ba40856ced59444c48fb34fb6db5e19
Thomas Radke <tradke@aei.mpg.de>
2006-02-07 12:04:00 +0000

    CarpetIOASCII: update the Carpet2ygraphCat.pl script
    The script now also supports scalar output from CarpetIOScalar and 0D output
    from CarpetIOASCII.
    
    darcs-hash:20060207120423-776a0-f9f6a928c292886ab617df32159ad04ae850bfed.gz

--- Carpet/CarpetIOASCII/src/util/Carpet2ygraphCat.pl
+++ Carpet/CarpetIOASCII/src/util/Carpet2ygraphCat.pl
@@ -1,35 +1,69 @@
-#! /usr/bin/perl -s
+#! /usr/bin/perl -sw
+#
+# Given an output file in CarpetIOScalar format or
+# CarpetIOASCII scalar or 1d format, strip to ygraph format.
+
+# Output is to a file named after the input file, with its extension ".asc"
+# substituted by ".xg".
+#
+# Example:
+#
+#   Carpet2ygraphCat.pl alp.x.asc
+#
+# produces alp.x.xg
+#
+#   Carpet2ygraph.pl alp.minimum.asc
+#
+# produces alp.minumum.xg
+#
+# Some headers of the Carpet files are just copied with the addition of
+# the correct time for ygraph, and the different comment marker.
+#
+# This script is an altered version of Ian Hawke's script.
+#
+
 
 use FileHandle;
 
 if(@ARGV != 1)
 {
-  print "Usage: $0 <Inputfile> \n";
+  print "Usage: $0 <Inputfile>\n";
   exit;
 }
 
-open(CARPETFILE,   "<$ARGV[0]") || die "Unable to find file \"$ARGV[0]\".";
-
-my $dirn;
-my $filename;
-$ARGV[0] =~ /(.*)\.([xyz])\..*/;
-$filename = $1; $dirn = $2;
 my $direction;
-if ($dirn eq "x") {
+my $filename = $ARGV[0];
+open(CARPETFILE, "< $filename") || die "Unable to find input file '$filename'\n.";
+
+if ($filename =~ /\.(x|y|z|d)\.asc$/) {
+  $direction = ord($1) - ord('x') + 9;
+  print "extracting CarpetIOASCII 1D data in direction $1...\n";
+} elsif ($filename =~ /\.\.asc$/) {
+  $direction = 8;
+  print "extracting CarpetIOASCII 0D data...\n";
+} elsif ($filename =~ /\.asc$/) {
   $direction = 0;
-}
-elsif ($dirn eq "y") {
-  $direction = 1;
-}
-elsif ($dirn eq "z") {
-  $direction = 2;
-}
-else {
-  die "Do not recognize direction \"${dirn}\" (file \"${ARGV[0]}\").";
+  print "extracting CarpetIOScalar data...\n";
+} else {
+  die "Do not recognize input file format for '$filename'.\n";
 }
 
-$file = $filename."_${dirn}.xg";
-my $fh = new FileHandle(">$file") || die "Unable to open file \"$file\".";
+$filename =~ s/asc$/xg/;
+my $fh = new FileHandle("> $filename") || die "Unable to open output file '$filename'.\n";
+
+# deal with scalar output
+if ($direction == 0) {
+  while (<CARPETFILE>) {
+    # The line is not a header comment
+    if(/^[0-9]/) {
+      my @data = split(/\s+/);
+      print $fh $data[1]." ".$data[2]."\n";
+    }
+  }
+  close ($fh);
+  exit;
+}
+
 
 my %data;
 my $time = -1;
@@ -45,50 +79,47 @@ my @lengths;
 $lengths[0]=0;
 while (<CARPETFILE>)
 {
-    $line = $_;
-    if ($line =~ "iteration") {
-      @itline = split(/ +/,$line);
-      $currentit = @itline[2];
+  chomp;
+  next if (/^$/);
+
+  if (/iteration/) {
+    @itline = split(/ +/);
+    $currentit = $itline[2];
+  }
+  #Do nothing for headers!
+  next if (/^#/);
+
+  @dataline = split(/[ \t]+/);
+  if ($currentit != $lastit) {
+    if ($new) {
+      # do not print "Time..." for zero-D data
+      push(@datatoprint,"\n\"Time = ".$time."\n") if ($direction !~ 8);
+
+      my @sortedcoords = sort numerically (keys %data);
+      foreach my $localcoord (@sortedcoords) {
+        push(@datatoprint, $localcoord." ".$data{$localcoord}."\n");
+      }
+      $maxlength = $maxlength > (scalar @sortedcoords) ? $maxlength : (scalar @sortedcoords);
+      $lengths[$nsets-1]=(scalar @sortedcoords);
+      $nsets++;
+      $lengths[$nsets-1]=0;
+      %data=();
     }
-    elsif ($line =~ /^#/)
-    {
-	#Do nothing for headers!
-    }
-    elsif ($line =~ /([0-9+-ed.]+)*/)
-    {
-	@dataline = split(/[ \t]+/,$line);
-	if ($currentit != $lastit)
-	{
-	    if ($new)
-	    {
-		push(@datatoprint,"\n\n\"Time = ".$time."\n");
-		my @sortedcoords = sort numerically (keys %data);
-		my $localcoord;
-		foreach $localcoord (@sortedcoords)
-		{
-		    push(@datatoprint, $localcoord." ".$data{$localcoord});
-		}
-		$maxlength = $maxlength > (scalar @sortedcoords) ? $maxlength : (scalar @sortedcoords);
-		$lengths[$nsets-1]=(scalar @sortedcoords);
-		$nsets++;
-		$lengths[$nsets-1]=0;
-		%data=();
-	    }
-	    $new++;
-	    $time = $dataline[8];
-	    $lastit = $currentit;
-	}
-	my $coord = $dataline[9+$direction];
-	my $val = $dataline[12];
-	$data{$coord} = $val;
-    }
+    $new++;
+    $time = $dataline[8];
+    $lastit = $currentit;
+  }
+  my $coord = $dataline[$direction];
+  my $val = $dataline[12];
+  $data{$coord} = $val;
 }
-push(@datatoprint, "\n\"Time = ",$time,"\n");
+
+# do not print "Time..." for zero-D data
+push(@datatoprint,"\n\"Time = ".$time."\n") if ($direction !~ 8);
+
 my @sortedcoords = sort numerically (keys %data);
-my $localcoord;
-foreach $localcoord (@sortedcoords)
-{
-    push(@datatoprint, $localcoord." ".$data{$localcoord});
+foreach my $localcoord (@sortedcoords) {
+  push(@datatoprint, $localcoord." ".$data{$localcoord}."\n");
 }
 $maxlength = $maxlength > (scalar @sortedcoords) ? $maxlength : (scalar @sortedcoords);
 $lengths[$nsets-1]=(scalar @sortedcoords);
@@ -102,8 +133,8 @@ foreach $line (@datatoprint) {
   if ($line =~ "Time") {
     if ($oldline) {
       for (my $i=$lengths[$set-1]; $i<$maxlength;$i++) {
-	$nouts++;
-	print $fh $oldline;
+        $nouts++;
+        print $fh $oldline;
       }
     }
     $set++;
@@ -115,9 +146,12 @@ foreach $line (@datatoprint) {
     $oldline=$line
   }
 }
-for (my $i=$lengths[$set-1]; $i<$maxlength;$i++) {
-  $nouts++;
-  print $fh $oldline;
-}
+
+# TR: I don't see why this is needed, it seems to print the last data item twice
+# in CarpetIOBasic 0D output
+#for (my $i=$lengths[$set-1]; $i<$maxlength;$i++) {
+#  $nouts++;
+#  print $fh $oldline;
+#}
 
 sub numerically {$a <=> $b;}
