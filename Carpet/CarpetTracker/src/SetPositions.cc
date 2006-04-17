8e005a7c4900b9a60e4987ea702b1d89fb4d6a23
Erik Schnetter <schnetter@cct.lsu.edu>
2006-04-17 22:18:00 +0000

    CarpetTracker: New thorn
    Track objects using SphericalSurface information and move the refined
    regions accordingly.  This thorn relies on CarpetRegrid2.
    
    darcs-hash:20060417221852-dae7b-b25e67ddd6f45dd2326ad34704f909b6ee626ffb.gz

---
+++ Carpet/CarpetTracker/src/SetPositions.cc
@@ -0,0 +1,65 @@
+#include <cassert>
+#include <sstream>
+#include <string>
+
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+
+
+
+namespace CarpetTracker {
+
+using namespace std;
+  
+  
+  
+  extern "C" {
+    void
+    CarpetTracker_SetPositions (CCTK_ARGUMENTS);
+  }
+  
+  
+  
+  void
+  SetParameter (char const * const dir, int const n, CCTK_REAL const pos)
+  {
+    ostringstream name_buf;
+    name_buf << "position_" << dir << "_" << n + 1;
+    string const name (name_buf.str());
+    ostringstream pos_buf;
+    pos_buf << pos;
+    string const pos_str (pos_buf.str());
+    int const ierr =
+      CCTK_ParameterSet (name.c_str(), "CarpetRegrid2", pos_str.c_str());
+    assert (not ierr);
+  }
+  
+  
+  
+  void
+  CarpetTracker_SetPositions (CCTK_ARGUMENTS)
+  {
+    DECLARE_CCTK_ARGUMENTS;
+    DECLARE_CCTK_PARAMETERS;
+    
+    for (int n = 0; n < 3; ++ n) {
+      int const sn = surface[n];
+      if (sn >= 0) {
+        assert (sn >= 0 and sn < nsurfaces);
+        
+        CCTK_VInfo (CCTK_THORNSTRING,
+                    "Setting position of refined region #%d from surface #%d to (%g,%g,%g)",
+                    n + 1, sn,
+                    static_cast <double> (sf_origin_x[sn]),
+                    static_cast <double> (sf_origin_y[sn]),
+                    static_cast <double> (sf_origin_z[sn]));
+        SetParameter ("x", n, sf_origin_x[sn]);
+        SetParameter ("y", n, sf_origin_y[sn]);
+        SetParameter ("z", n, sf_origin_z[sn]);
+        
+      } // if
+    } // for
+  }
+  
+} // namespace CarpetTracker
