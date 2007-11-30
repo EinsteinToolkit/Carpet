5c0bf04f3372ebd784d5f99083781b0cda21f977
cott <schnetter@cct.lsu.edu>
2007-11-30 05:21:00 +0000

    Temporary hack to fix hdf5-1.8.0-beta5 compatibility issues
    darcs-hash:20071130052144-fff0f-57258dcf6536be5b2d8a14f2a5ef5be56d6f038f.gz

--- Carpet/CarpetIOHDF5/src/CarpetIOHDF5.hh
+++ Carpet/CarpetIOHDF5/src/CarpetIOHDF5.hh
@@ -1,11 +1,27 @@
 #ifndef CARPETIOHDF5_HH
 #define CARPETIOHDF5_HH
 
+// some macros to fix compatibility issues as long
+// as 1.8.0 is in beta phase
+#define H5_USE_16_API 1
 
 #include <hdf5.h>
+
+#if (H5_VERS_MAJOR == 1 && (H5_VERS_MINOR == 8) && (H5_VERS_RELEASE == 0))
+#warning "Hacking HDF5 1.8.0 compatiblity with 1.6.x; fix once 1.8.0 stable"
+#warning "Hacking HDF5 1.8.0 compatiblity with 1.6.x; fix once 1.8.0 stable"
+#warning "Hacking HDF5 1.8.0 compatiblity with 1.6.x; fix once 1.8.0 stable"
+#warning "Hacking HDF5 1.8.0 compatiblity with 1.6.x; fix once 1.8.0 stable"
+#warning "Hacking HDF5 1.8.0 compatiblity with 1.6.x; fix once 1.8.0 stable"
+#endif
+
+
 #include "cctk_Arguments.h"
 #include "CactusBase/IOUtil/src/ioutil_Utils.h"
 #include "carpet.hh"
+
+
+
 
 
 // some macros for HDF5 group names
