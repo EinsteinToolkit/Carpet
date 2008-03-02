a888ce29b9e5481abb1fec50d41238bd404577e2
Erik Schnetter <schnetter@cct.lsu.edu>
2008-03-01 22:24:02 -0600

    New thorn CarpetExtra/ReductionTest2

---
+++ CarpetExtra/ReductionTest2/src/CoM.c
@@ -0,0 +1,28 @@
+#include "cctk.h"
+#include "cctk_Parameters.h"
+#include "cctk_Arguments.h"
+#include "util_Table.h"
+#include <assert.h>
+#include <math.h>
+
+void CoM2_Local(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  int i,j,k,index;
+
+  int nx = cctk_lsh[0];
+  int ny = cctk_lsh[1];
+  int nz = cctk_lsh[2];
+
+  for (k=0;k<nz;k++)
+    for (j=0;j<ny;j++)
+      for (i=0;i<nx;i++) {
+
+	index = CCTK_GFINDEX3D(cctkGH,i,j,k);
+
+	redvar[index] = 0.45e0;
+
+      }
+}
