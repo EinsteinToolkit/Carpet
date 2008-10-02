3d18e299788c753ae99866b52f916346f5c91039
Erik Schnetter <schnetter@cct.lsu.edu>
2008-10-02 11:50:19 -0500

    CarpetMask: Allow excluding the exterior of large coordinate spheres
    Allow excluding the exterior of large coordinate spheres from norm
    calculations.

--- Carpet/CarpetMask/src/mask_excluded.cc
+++ Carpet/CarpetMask/src/mask_excluded.cc
@@ -44,6 +44,8 @@ namespace CarpetMask {
         
         CCTK_REAL const r2 = pow (r0, 2);
         
+        bool const exterior = exclude_exterior[n];
+        
         for (int k = 0; k < cctk_lsh[2]; ++ k) {
           for (int j = 0; j < cctk_lsh[1]; ++ j) {
             for (int i = 0; i < cctk_lsh[0]; ++ i) {
@@ -53,7 +55,10 @@ namespace CarpetMask {
               CCTK_REAL const dy2 = pow (y[ind] - y0, 2);
               CCTK_REAL const dz2 = pow (z[ind] - z0, 2);
               
-              if (dx2 + dy2 + dz2 <= r2) {
+              if (exterior ?
+                  dx2 + dy2 + dz2 >= r2 :
+                  dx2 + dy2 + dz2 <= r2)
+              {
                 weight[ind] = 0.0;
               }
               
