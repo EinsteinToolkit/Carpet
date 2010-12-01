eb6f949dec570d440ffffe8429d5213675c697e4
Erik Schnetter <schnetter@cct.lsu.edu>
2010-12-01 16:40:26 -0500

    CarpetIOF5: Make LoopControl optional

--- CarpetDev/CarpetIOF5/src/poison.cc
+++ CarpetDev/CarpetIOF5/src/poison.cc
@@ -29,16 +29,13 @@ namespace CarpetIOF5 {
       BEGIN_LOCAL_MAP_LOOP(cctkGH, CCTK_GF) {
         BEGIN_LOCAL_COMPONENT_LOOP(cctkGH, CCTK_GF) {
           DECLARE_CCTK_ARGUMENTS;
-          LC_LOOP3(F5_Poison, i,j,k,
-                   0,0,0, cctk_lsh[0],cctk_lsh[1],cctk_lsh[2],
-                   cctk_lsh[0],cctk_lsh[1],cctk_lsh[2])
-          {
+          CCTK_LOOP3_ALL(F5_Poison, cctkGH, i,j,k) {
             int const ind3d = CCTK_GFINDEX3D(cctkGH, i,j,k);
             x[ind3d] = nan;
             y[ind3d] = nan;
             z[ind3d] = nan;
             r[ind3d] = nan;
-          } LC_ENDLOOP3(F5_Poison);
+          } CCTK_ENDLOOP3_ALL(F5_Poison);
         } END_LOCAL_COMPONENT_LOOP;
       } END_LOCAL_MAP_LOOP;
     } END_REFLEVEL_LOOP;
@@ -59,16 +56,13 @@ namespace CarpetIOF5 {
       BEGIN_LOCAL_MAP_LOOP(cctkGH, CCTK_GF) {
         BEGIN_LOCAL_COMPONENT_LOOP(cctkGH, CCTK_GF) {
           DECLARE_CCTK_ARGUMENTS;
-          LC_LOOP3(F5_Check, i,j,k,
-                   0,0,0, cctk_lsh[0],cctk_lsh[1],cctk_lsh[2],
-                   cctk_lsh[0],cctk_lsh[1],cctk_lsh[2])
-          {
+          CCTK_LOOP3_ALL(F5_Check, cctkGH, i,j,k) {
             int const ind3d = CCTK_GFINDEX3D(cctkGH, i,j,k);
             assert (not isnan(x[ind3d]));
             assert (not isnan(y[ind3d]));
             assert (not isnan(z[ind3d]));
             assert (not isnan(r[ind3d]));
-          } LC_ENDLOOP3(F5_Check);
+          } CCTK_ENDLOOP3_ALL(F5_Check);
         } END_LOCAL_COMPONENT_LOOP;
       } END_LOCAL_MAP_LOOP;
     } END_REFLEVEL_LOOP;
