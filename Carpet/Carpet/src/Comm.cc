1f56f8e306d5d5c4d53eeac5be70c8e66ab2d490
Erik Schnetter <schnetter@cct.lsu.edu>
2010-12-22 20:37:26 -0600

    Carpet: Increase warning level for synchronising groups without storage

--- Carpet/Carpet/src/Comm.cc
+++ Carpet/Carpet/src/Comm.cc
@@ -118,7 +118,7 @@ namespace Carpet {
       }
 
       if (not CCTK_QueryGroupStorageI (cctkGH, g)) {
-        CCTK_VWarn (2, __LINE__, __FILE__, CCTK_THORNSTRING,
+        CCTK_VWarn (4, __LINE__, __FILE__, CCTK_THORNSTRING,
                     "Cannot synchronise group \"%s\" because it has no storage",
                     groupname);
         retval = -1;
