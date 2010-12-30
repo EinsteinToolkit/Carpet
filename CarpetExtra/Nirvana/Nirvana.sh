7367d17e7a4730b09ee26cbd85925359c03921ed
Erik Schnetter <schnetter@cct.lsu.edu>
2010-12-30 12:31:14 -0500

    Nirvana: Update library and Modernise script

--- CarpetExtra/Nirvana/Nirvana.sh
+++ CarpetExtra/Nirvana/Nirvana.sh
@@ -19,21 +19,20 @@ echo "Building Nirvana..."
 echo "END MESSAGE"
 
 # Set locations
+THORN=Nirvana
 NAME=Nirvana
 SRCDIR=$(dirname $0)
-INSTALL_DIR=${SCRATCH_BUILD}
-NIRVANA_DIR=${INSTALL_DIR}/build-${NAME}/${NAME}
+INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
+DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
+NIRVANA_DIR=${INSTALL_DIR}
 
-# Set up environment
-unset LIBS
-    
 (
     exec >&2                    # Redirect stdout to stderr
     set -x                      # Output commands
     set -e                      # Abort on errors
-    cd ${INSTALL_DIR}
-    if [ -e done-${NAME} -a done-${NAME} -nt ${SRCDIR}/dist/${NAME}.tar.gz \
-                         -a done-${NAME} -nt ${SRCDIR}/Nirvana.sh ]
+    cd ${SCRATCH_BUILD}
+    if [ -e ${DONE_FILE} -a ${DONE_FILE} -nt ${SRCDIR}/dist/${NAME}.tar.gz \
+                         -a ${DONE_FILE} -nt ${SRCDIR}/Nirvana.sh ]
     then
         echo "Nirvana: The enclosed Nirvana library has already been built; doing nothing"
     else
@@ -42,15 +41,20 @@ unset LIBS
         # Should we use gtar or tar?
         TAR=$(gtar --help > /dev/null 2> /dev/null && echo gtar || echo tar)
         
+        # Set up environment
+        unset LIBS
+        
+        echo "Nirvana: Preparing directory structure..."
+        mkdir external done 2> /dev/null || true
+        rm -rf ${INSTALL_DIR}
+        mkdir ${INSTALL_DIR}
+        
         echo "Nirvana: Unpacking archive..."
-        rm -rf build-${NAME}
-        mkdir build-${NAME}
-        pushd build-${NAME}
+        pushd ${INSTALL_DIR}
         ${TAR} xzf ${SRCDIR}/dist/${NAME}.tar.gz
-        popd
         
         echo "Nirvana: Building..."
-        pushd build-${NAME}/${NAME}
+        cd ${NAME}
         ${CXX} ${CPPFLAGS} ${CXXFLAGS} -c *.cc $(for dir in ${HDF5_INC_DIRS}; do echo -I${dir}; done)
         ${AR} ${ARFLAGS} libNirvana.a *.o
 	if [ ${USE_RANLIB} = 'yes' ]; then
@@ -58,14 +62,14 @@ unset LIBS
         fi
         popd
         
-        echo 'done' > done-${NAME}
+        date > ${DONE_FILE}
         echo "Nirvana: Done."
     fi
 )
     
 if (( $? )); then
     echo 'BEGIN ERROR'
-    echo 'Error while building Nirvana.  Aborting.'
+    echo 'Error while building Nirvana. Aborting.'
     echo 'END ERROR'
     exit 1
 fi
@@ -73,8 +77,8 @@ fi
 
 
 # Set options
-NIRVANA_INC_DIRS="${NIRVANA_DIR}"
-NIRVANA_LIB_DIRS="${NIRVANA_DIR}"
+NIRVANA_INC_DIRS="${NIRVANA_DIR}/Nirvana"
+NIRVANA_LIB_DIRS="${NIRVANA_DIR}/Nirvana"
 NIRVANA_LIBS='Nirvana'
 
 
