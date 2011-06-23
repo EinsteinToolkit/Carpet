549447bb134500f38da40957c4c52753a1459532
Erik Schnetter <schnetter@cct.lsu.edu>
2011-06-23 11:04:49 -0400

    CarpetRegrid2: Enforce certain grid hierarchy properties
    Enforce certain grid structure properites, and ensure that they hold
    after being enforced.
    
    This change may lead to Carpet using a (slightly) different grid
    structure than before. The differences are not "important"; what
    potentially changes is the order in which consistency conditions are
    applied. I expect that, in most cases, there will be no differences.
    The list of consistency conditions is:
    
    - ensure proper nesting (level must be larger than next finer level)
    - add buffer zones (only once, not really a consistency condition)
    - combine regions into a single box (if this is efficient)
    - apply rotating 90/180 symmetry (if requested)
    - clip at outer boundary (so that boxes that are too large or are
      outside are cut off)
    
    This change also reorganises the code in CarpetRegrid2.

---
+++ Carpet/CarpetRegrid2/src/property.hh
@@ -0,0 +1,162 @@
+#ifndef PROPERTY_HH
+#define PROPERTY_HH
+
+// Consistency properties for the grid structure
+
+#include <vector>
+
+#include <bboxset.hh>
+#include <defs.hh>
+#include <dh.hh>
+#include <gh.hh>
+
+
+
+namespace CarpetRegrid2 {
+  
+  
+  
+  // Each property consists of a test, which returns true or false
+  // depending on whether the property is satisfied, and an action
+  // that enforces the property.
+  class property {
+  protected:
+    virtual bool test_impl (gh const& hh, dh const& dd,
+                            level_boundary const& bnd,
+                            vector<ibset> const& regions, int rl) = 0;
+    virtual void enforce_impl (gh const& hh, dh const& dd,
+                               level_boundary const& bnd,
+                               vector<ibset>& regions, int rl) = 0;
+  public:
+    bool test (gh const& hh, dh const& dd,
+               level_boundary const& bnd,
+               vector<ibset> const& regions, int rl);
+    void enforce (gh const& hh, dh const& dd,
+                  level_boundary const& bnd,
+                  vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Ensure that this grid contains the next finer grid
+  class proper_nesting: public property {
+    ibset enlarged_fine_grid (gh const& hh, dh const& dd,
+                              level_boundary const& bnd,
+                              vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                     vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Add buffer zones (do this only once)
+  class add_buffers: public property {
+    ibset buffered_regions (gh const& hh, dh const& dd,
+                            level_boundary const& bnd,
+                            vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Combine all regions into a single region, if this is worthwhile
+  class combine_regions: public property {
+    ibbox combined_regions (gh const& hh, dh const& dd,
+                            level_boundary const& bnd,
+                            vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Align the boxes with the next coarser grid
+  class snap_coarse: public property {
+    ibset snapped_regions (gh const& hh, dh const& dd,
+                           level_boundary const& bnd,
+                           vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Make the boxes rotating-90 symmetric
+  class rotsym90: public property {
+    ibset symmetrised_regions (gh const& hh, dh const& dd,
+                               level_boundary const& bnd,
+                               vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Make the boxes rotating-180 symmetric
+  class rotsym180: public property {
+    ibset symmetrised_regions (gh const& hh, dh const& dd,
+                               level_boundary const& bnd,
+                               vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Clip at the outer boundary
+  class boundary_clip: public property {
+    ibset clipped_regions (gh const& hh, dh const& dd,
+                           level_boundary const& bnd,
+                           vector<ibset> const& regions, int rl);
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+  // Ensure that this grid is contained in the domain
+  class in_domain: public property {
+    bool test_impl (gh const& hh, dh const& dd,
+                    level_boundary const& bnd,
+                    vector<ibset> const& regions, int rl);
+    void enforce_impl (gh const& hh, dh const& dd,
+                       level_boundary const& bnd,
+                       vector<ibset>& regions, int rl);
+  };
+  
+  
+  
+} // namespace CarpetRegrid2
+
+
+
+#endif  // #ifndef PROPERTY_HH
