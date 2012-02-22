07d12df03f216b25fef1d4b930cb3b1e9e68c108
Roland Haas <roland.haas@physics.gatech.edu>
2012-02-22 07:59:46 -0800

    CarpetIOBasic: Add output criterion "divisor" Support output criterion "divisor" in CarpetIOBasic. ---  Carpet/CarpetIOBasic/param.ccl      |    1 +  Carpet/CarpetIOBasic/src/iobasic.cc |   17 +++++++++++++++++  2 files changed, 18 insertions(+), 0 deletions(-)

--- Carpet/CarpetIOBasic/src/iobasic.cc
+++ Carpet/CarpetIOBasic/src/iobasic.cc
@@ -562,6 +562,23 @@ namespace CarpetIOBasic {
         output_this_iteration = false;
       }
 
+    } else if (CCTK_EQUALS (myoutcriterion, "divisor")) {
+
+      int myoutevery = outInfo_every;
+      if (myoutevery == -2) {
+        myoutevery = out_every;
+      }
+      if (myoutevery <= 0) {
+        // output is disabled
+        output_this_iteration = false;
+      } else if ((cctk_iteration % myoutevery) == 0 ) {
+        // we already decided to output this iteration
+        output_this_iteration = true;
+      } else {
+        // we want no output at this iteration
+        output_this_iteration = false;
+      }
+
     } else if (CCTK_EQUALS (myoutcriterion, "time")) {
 
       CCTK_REAL myoutdt = outInfo_dt;
