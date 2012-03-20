95d0971c90c22ad1a020dcd3bc3facee4036e19a
Roland Haas <rhaas@caltech.edu>
2012-03-20 08:48:47 -0700

    Carpet: replace omp_get_num_threads() by mynthreads to compile without OpenMP

--- Carpet/Carpet/src/SetupGH.cc
+++ Carpet/Carpet/src/SetupGH.cc
@@ -356,7 +356,7 @@ namespace Carpet {
           {
             cpu_set_t cpumask;
             CPU_ZERO(&cpumask);
-            CPU_SET(n0 + omp_get_thread_num(), &cpumask);
+            CPU_SET(n0 + mynthreads, &cpumask);
             int const ierr = sched_setaffinity(0, sizeof(cpumask), &cpumask);
             assert (not ierr);
           }
