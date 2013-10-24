ae71ad5e952a892c7b4a326daa078bccf6e4393b
Roland Haas <roland.haas@physics.gatech.edu>
2013-10-24 00:54:15 -0700

    LoopControl: provide omp_get_max_threads fallback

--- Carpet/LoopControl/src/loopcontrol.cc
+++ Carpet/LoopControl/src/loopcontrol.cc
@@ -24,6 +24,7 @@
 #else
 
 // Simple non-OpenMP implementations
+static inline int omp_get_max_threads() { return 1; }
 static inline int omp_get_num_threads() { return 1; }
 static inline int omp_get_thread_num() { return 0; }
 
