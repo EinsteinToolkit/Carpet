ff87da44fab3c385c79cd92c68d619c694025a20
Erik Schnetter <schnetter@gmail.com>
2013-12-05 13:19:02 -0500

    CarpetLib: dh.cc: white space change

--- Carpet/CarpetLib/src/dh.cc
+++ Carpet/CarpetLib/src/dh.cc
@@ -1056,7 +1056,7 @@ regrid (bool const do_init)
           full_dboxes const& obox = full_olevel.AT(oc);
           
           ibset needrecv = allrestricted & obox.owned;
-          if(use_higher_order_restriction) {
+          if (use_higher_order_restriction) {
             // NOTE: change in behaviour (affects only outer boundaries I think)!!!!
             // NOTE: b/c of this we need a low-level sync after the restrict
             needrecv = allrestricted & obox.interior;
@@ -1072,7 +1072,7 @@ regrid (bool const do_init)
             // test in the actual operator. 
             int const shrink_by =
               use_higher_order_restriction and h.refcent == cell_centered ?
-              restriction_order_space/2  : 0;
+              restriction_order_space/2 : 0;
             ibbox const contracted_exterior = 
               box.exterior.expand(ivect(-shrink_by)).contracted_for(odomext);
             ibset const ovlp = needrecv & contracted_exterior;
