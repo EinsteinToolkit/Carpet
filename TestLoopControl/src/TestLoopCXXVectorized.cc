51a9911681c69c423c1e1c9022790e5a53e5a83c
Erik Schnetter <schnetter@gmail.com>
2015-08-21 10:20:00 -0400

    TestLoopControl: Simplify and correct tests

--- TestLoopControl/src/TestLoopCXXVectorized.cc
+++ TestLoopControl/src/TestLoopCXXVectorized.cc
@@ -25,12 +25,24 @@ void TestLoopControlCXXVectorized(CCTK_ARGUMENTS)
   int const alignment = stride * sizeof(CCTK_REAL);
   
   // We allocate a new grid function to ensure it is aligned. We
-  // allocate "stride" more elements so that we can safely align
-  // upwards.
-  vector<CCTK_REAL> r2m(cctk_ash[0] * cctk_ash[1] * cctk_ash[2] + stride);
+  // allocate more elements so that we can safely align upwards, and
+  // can access additional elements past the end.
+  size_t const npoints = cctk_ash[0] * cctk_ash[1] * cctk_ash[2];
+  size_t const last_index =
+    CCTK_GFINDEX3D(cctkGH, cctk_lsh[0]-1, cctk_lsh[1]-1, cctk_lsh[2]-1);
+  vector<unsigned char>
+    r2m(sizeof(CCTK_REAL) * (npoints + stride-1) + alignment-1);
+  size_t const r2ptr = size_t(&r2m[0]);
   // Align grid function
-  CCTK_REAL *restrict const r2 =
-    (CCTK_REAL*)(size_t(&r2m[stride]) / alignment * alignment);
+  size_t const r2ptr_aligned = (r2ptr + alignment-1) / alignment * alignment;
+  assert(r2ptr_aligned >= r2ptr);
+  assert(r2ptr_aligned < r2ptr + alignment);
+  assert(r2ptr_aligned % alignment == 0);
+  // Convert pointer
+  CCTK_REAL *restrict const r2 = (CCTK_REAL*)r2ptr_aligned;
+  assert((unsigned char*)&r2[0] >= &r2m.begin()[0]);
+  assert((unsigned char*)&r2[last_index + stride] <= &r2m.end()[0]);
+  assert(size_t(&r2[0]) % alignment == 0);
   
 #pragma omp parallel
   CCTK_LOOP3_ALL(copy, cctkGH, i,j,k) {
@@ -38,73 +50,87 @@ void TestLoopControlCXXVectorized(CCTK_ARGUMENTS)
     r2[ind3d] = r[ind3d];
   } CCTK_ENDLOOP3_ALL(copy);
   
-  bool alignment_error = false;
-  
-  *cxxsum_all = 0.0;
-#pragma omp parallel reduction(||: alignment_error)
+  CCTK_REAL sum_all = 0.0;
+#pragma omp parallel reduction(+: sum_all)
   CCTK_LOOP3STR_ALL(loop3str_all, cctkGH, i,j,k, imin,imax, stride) {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, imin,j,k) + i-imin;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxxsum_all += r2[ind3d+v];
+        any_m = true;
+        sum_all += r2[ind3d+v];
       }
     }
+    assert(any_m);
   } CCTK_ENDLOOP3STR_ALL(loop3str_all);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+  *cxxsum_all = sum_all;
   
-  *cxxsum_int = 0.0;
-#pragma omp parallel
+  CCTK_REAL sum_int = 0.0;
+#pragma omp parallel reduction(+: sum_int)
   CCTK_LOOP3STR_INT(loop3str_int, cctkGH, i,j,k, imin,imax, stride) {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, imin,j,k) + i-imin;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxxsum_int += r2[ind3d+v];
+        any_m = true;
+        sum_int += r2[ind3d+v];
       }
     }
+    assert(any_m);
   } CCTK_ENDLOOP3STR_INT(loop3str_int);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+  *cxxsum_int = sum_int;
   
-  *cxxsum_bnd = 0.0;
-#pragma omp parallel
+  CCTK_REAL sum_bnd = 0.0;
+#pragma omp parallel reduction(+: sum_bnd)
   CCTK_LOOP3STR_BND(loop3str_bnd, cctkGH, i,j,k, ni,nj,nk, imin,imax, stride) {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, imin,j,k) + i-imin;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxxsum_bnd += r2[ind3d+v];
+        any_m = true;
+        sum_bnd += r2[ind3d+v];
       }
     }
+    assert(any_m);
   } CCTK_ENDLOOP3STR_BND(loop3str_bnd);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+  *cxxsum_bnd = sum_bnd;
   
-  *cxxsum_intbnd = 0.0;
-#pragma omp parallel
+  CCTK_REAL sum_intbnd = 0.0;
+#pragma omp parallel reduction(+: sum_intbnd)
   CCTK_LOOP3STR_INTBND(loop3str_intbnd,
                        cctkGH, i,j,k, ni,nj,nk, imin,imax, stride)
   {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, imin,j,k) + i-imin;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxxsum_intbnd += r2[ind3d+v];
+        any_m = true;
+        sum_intbnd += r2[ind3d+v];
       }
     }
+    assert(any_m);
   } CCTK_ENDLOOP3STR_INTBND(loop3str_intbnd);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+  *cxxsum_intbnd = sum_intbnd;
 }
 
 
@@ -123,19 +149,34 @@ void TestLoopControlCXXVectorized2(CCTK_ARGUMENTS)
   int const alignment = stride * sizeof(CCTK_REAL);
   
   // We allocate a new grid function to ensure it is aligned. We
-  // allocate "stride" more elements so that we can safely align
-  // upwards, and we align another "stride" more elements to ensure we
-  // can access dummy elements before the first.
-  vector<CCTK_REAL> r2m(cctk_ash[0] * cctk_ash[1] * cctk_ash[2] + 2*stride);
+  // allocate more elements so that we can safely align upwards, and
+  // can access additional elements before the beginning and past the
+  // end.
+  size_t const npoints = cctk_ash[0] * cctk_ash[1] * cctk_ash[2];
+  size_t const interior_index = CCTK_GFINDEX3D(cctkGH,
+                                               cctk_nghostzones[0],
+                                               cctk_nghostzones[1],
+                                               cctk_nghostzones[2]);
+  size_t const last_index =
+    CCTK_GFINDEX3D(cctkGH, cctk_lsh[0]-1, cctk_lsh[1]-1, cctk_lsh[2]-1);
+  vector<unsigned char>
+    r2m(sizeof(CCTK_REAL) * (npoints + 2*(stride-1)) + alignment-1);
+  size_t const r2ptr = size_t(&r2m[0]);
   // Offset of the first interior point
-  int const int_off = CCTK_GFINDEX3D(cctkGH,
-                                     cctk_nghostzones[0],
-                                     cctk_nghostzones[1],
-                                     cctk_nghostzones[2]);
+  size_t const int_off = sizeof(CCTK_REAL) * interior_index;
+  // Add space before first grid point
+  size_t const r2ptr2 = r2ptr + sizeof(CCTK_REAL) * (stride-1);
   // Align grid function
-  CCTK_REAL *restrict const r2 =
-    (CCTK_REAL*)(size_t(&r2m[int_off + 2*stride]) / alignment * alignment) -
-    int_off;
+  size_t const r2ptr_aligned =
+    (r2ptr2 + alignment-1 + int_off) / alignment * alignment - int_off;
+  assert(r2ptr_aligned >= r2ptr2);
+  assert(r2ptr_aligned < r2ptr2 + alignment);
+  assert((r2ptr_aligned + int_off) % alignment == 0);
+  // Convert pointer
+  CCTK_REAL *restrict const r2 = (CCTK_REAL*)r2ptr_aligned;
+  assert((unsigned char*)&r2[-(stride-1)] >= &r2m.begin()[0]);
+  assert((unsigned char*)&r2[last_index + stride] <= &r2m.end()[0]);
+  assert(size_t(&r2[interior_index]) % alignment == 0);
   
 #pragma omp parallel
   CCTK_LOOP3_ALL(copy, cctkGH, i,j,k) {
@@ -145,77 +186,91 @@ void TestLoopControlCXXVectorized2(CCTK_ARGUMENTS)
   
   int const offset = size_t(r2) / sizeof(CCTK_REAL) % stride;
   
-  bool alignment_error = false;
-  
-  *cxx2sum_all = 0.0;
-#pragma omp parallel reduction(||: alignment_error)
-  CCTK_LOOP3STRMOD_ALL(loop3strmod_all,
+  CCTK_REAL sum_all = 0.0;
+#pragma omp parallel reduction(+: sum_all)
+  CCTK_LOOP3STROFF_ALL(loop3stroff_all,
                        cctkGH, i,j,k, imin,imax, stride,offset)
   {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, 0,j,k) + i;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxx2sum_all += r2[ind3d+v];
+        any_m = true;
+        sum_all += r2[ind3d+v];
       }
     }
-  } CCTK_ENDLOOP3STRMOD_ALL(loop3strmod_all);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+    assert(any_m);
+  } CCTK_ENDLOOP3STROFF_ALL(loop3stroff_all);
+  *cxx2sum_all = sum_all;
   
-  *cxx2sum_int = 0.0;
-#pragma omp parallel reduction(||: alignment_error)
-  CCTK_LOOP3STRMOD_INT(loop3strmod_int,
+  CCTK_REAL sum_int = 0.0;
+#pragma omp parallel reduction(+: sum_int)
+  CCTK_LOOP3STROFF_INT(loop3stroff_int,
                        cctkGH, i,j,k, imin,imax, stride,offset)
   {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, 0,j,k) + i;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxx2sum_int += r2[ind3d+v];
+        any_m = true;
+        sum_int += r2[ind3d+v];
       }
     }
-  } CCTK_ENDLOOP3STRMOD_INT(loop3strmod_int);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+    assert(any_m);
+  } CCTK_ENDLOOP3STROFF_INT(loop3stroff_int);
+  *cxx2sum_int = sum_int;
   
-  *cxx2sum_bnd = 0.0;
-#pragma omp parallel reduction(||: alignment_error)
-  CCTK_LOOP3STRMOD_BND(loop3strmod_bnd,
+  CCTK_REAL sum_bnd = 0.0;
+#pragma omp parallel reduction(+: sum_bnd)
+  CCTK_LOOP3STROFF_BND(loop3stroff_bnd,
                        cctkGH, i,j,k, ni,nj,nk, imin,imax, stride,offset)
   {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, 0,j,k) + i;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxx2sum_bnd += r2[ind3d+v];
+        any_m = true;
+        sum_bnd += r2[ind3d+v];
       }
     }
-  } CCTK_ENDLOOP3STRMOD_BND(loop3strmod_bnd);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+    assert(any_m);
+  } CCTK_ENDLOOP3STROFF_BND(loop3stroff_bnd);
+  *cxx2sum_bnd = sum_bnd;
   
-  *cxx2sum_intbnd = 0.0;
-#pragma omp parallel reduction(||: alignment_error)
-  CCTK_LOOP3STRMOD_INTBND(loop3strmod_intbnd,
+  CCTK_REAL sum_intbnd = 0.0;
+#pragma omp parallel reduction(+: sum_intbnd)
+  CCTK_LOOP3STROFF_INTBND(loop3stroff_intbnd,
                           cctkGH, i,j,k, ni,nj,nk, imin,imax, stride,offset)
   {
     int const ind3d = CCTK_GFINDEX3D(cctkGH, 0,j,k) + i;
-    alignment_error = alignment_error || size_t(&r2[ind3d]) % alignment != 0;
+    bool const alignment_error = size_t(&r2[ind3d]) % alignment != 0;
+    if (alignment_error) {
+      CCTK_ERROR("Alignment error");
+    }
+    bool any_m = false;
     for (int v=0; v<stride; ++v) {
       bool const m = i+v>=imin && i+v<imax;
       if (m) {
-        *cxx2sum_intbnd += r2[ind3d+v];
+        any_m = true;
+        sum_intbnd += r2[ind3d+v];
       }
     }
-  } CCTK_ENDLOOP3STRMOD_INTBND(loop3strmod_intbnd);
-  if (alignment_error) {
-    CCTK_ERROR("Alignment error");
-  }
+    assert(any_m);
+  } CCTK_ENDLOOP3STROFF_INTBND(loop3stroff_intbnd);
+  *cxx2sum_intbnd = sum_intbnd;
 }
