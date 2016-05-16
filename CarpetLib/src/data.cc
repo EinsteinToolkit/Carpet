EinsteinToolkit/Carpet

commit fe8d0d5b91c4ebb6d63971e356d2e3926e65030d

CarpetLib: New option use_openmp
This option can be used to disable OpenMP parallelization.

CarpetLib/src/data.cc
@@ -52,11 +52,7 @@ call_operator(void (*the_operator)(
   if (use_loopcontrol_in_operators) {
     (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
                     dstbbox, srcregbbox, dstregbbox, extraargs);
-  } else {
-#ifndef _OPENMP
-    (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
-                    dstbbox, srcregbbox, dstregbbox, extraargs);
-#else
+  } else if (use_openmp) {
 #ifdef CARPET_DEBUG
     ibset alldstregbboxes;
 #endif
@@ -120,54 +116,59 @@ call_operator(void (*the_operator)(
               ibbox4 const &restrict srcbbox, ibbox4 const &restrict dstbbox,
               ibbox4 const &restrict srcregbbox,
               ibbox4 const &restrict dstregbbox, void *const extraargs) {
-#ifndef _OPENMP
-  (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
-                  dstbbox, srcregbbox, dstregbbox, extraargs);
-#else
-#if !defined(NDEBUG) && !defined(CARPET_OPTIMISE)
-  ibset4 alldstregbboxes;
+  DECLARE_CCTK_PARAMETERS;
+
+  if (use_loopcontrol_in_operators) {
+    (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
+                    dstbbox, srcregbbox, dstregbbox, extraargs);
+  } else if (use_openmp) {
+#ifdef CARPET_DEBUG
+    ibset4 alldstregbboxes;
 #endif
 #pragma omp parallel
-  {
-    int const num_threads = omp_get_num_threads();
-    int const thread_num = omp_get_thread_num();
-    // Parallelise in z direction
-    // int const dir = 2;
-    // Parallelise along longest extent
-    int const dir = maxloc(dstregbbox.shape());
-    int const stride = dstregbbox.stride()[dir];
-    int const first_point = dstregbbox.lower()[dir];
-    int const last_point = dstregbbox.upper()[dir] + stride;
-    int const num_points = last_point - first_point;
-    assert(num_points >= 0);
-    assert(num_points % stride == 0);
-    int const my_num_points =
-        (num_points / stride + num_threads - 1) / num_threads * stride;
-    int const my_first_point =
-        min(last_point, first_point + thread_num * my_num_points);
-    int const my_last_point = min(last_point, my_first_point + my_num_points);
-    assert(my_last_point >= my_first_point);
-    ibbox4 const mydstregbbox(
-        dstregbbox.lower().replace(dir, my_first_point),
-        dstregbbox.upper().replace(dir, my_last_point - stride),
-        dstregbbox.stride());
-    if (not mydstregbbox.empty()) {
-      (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
-                      dstbbox, srcregbbox, mydstregbbox, extraargs);
-#if !defined(NDEBUG) && !defined(CARPET_OPTIMISE)
+    {
+      int const num_threads = omp_get_num_threads();
+      int const thread_num = omp_get_thread_num();
+      // Parallelise in z direction
+      // int const dir = 2;
+      // Parallelise along longest extent
+      int const dir = maxloc(dstregbbox.shape());
+      int const stride = dstregbbox.stride()[dir];
+      int const first_point = dstregbbox.lower()[dir];
+      int const last_point = dstregbbox.upper()[dir] + stride;
+      int const num_points = last_point - first_point;
+      assert(num_points >= 0);
+      assert(num_points % stride == 0);
+      int const my_num_points =
+          (num_points / stride + num_threads - 1) / num_threads * stride;
+      int const my_first_point =
+          min(last_point, first_point + thread_num * my_num_points);
+      int const my_last_point = min(last_point, my_first_point + my_num_points);
+      assert(my_last_point >= my_first_point);
+      ibbox4 const mydstregbbox(
+          dstregbbox.lower().replace(dir, my_first_point),
+          dstregbbox.upper().replace(dir, my_last_point - stride),
+          dstregbbox.stride());
+      if (not mydstregbbox.empty()) {
+        (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
+                        dstbbox, srcregbbox, mydstregbbox, extraargs);
+#ifdef CARPET_DEBUG
 #pragma omp critical
-      alldstregbboxes += mydstregbbox;
+        alldstregbboxes += mydstregbbox;
 #endif
+      }
     }
-  }
-#if !defined(NDEBUG) && !defined(CARPET_OPTIMISE)
-  if (not(alldstregbboxes == ibset4(dstregbbox))) {
-    cout << "alldstregbboxes=" << alldstregbboxes << endl
-         << "dstregbbox=" << dstregbbox << endl;
-  }
-  assert(alldstregbboxes == ibset4(dstregbbox));
+#ifdef CARPET_DEBUG
+    if (not(alldstregbboxes == ibset4(dstregbbox))) {
+      cout << "alldstregbboxes=" << alldstregbboxes << endl
+           << "dstregbbox=" << dstregbbox << endl;
+    }
+    assert(alldstregbboxes == ibset4(dstregbbox));
 #endif
-#endif
+  } else { // serial
+    (*the_operator)(src, srcpadext, srcext, dst, dstpadext, dstext, srcbbox,
+                    dstbbox, srcregbbox, dstregbbox, extraargs);
+  }
 }
 
 // Fortran wrappers

CarpetLib/param.ccl
@@ -43,6 +43,10 @@ BOOLEAN use_loopcontrol_in_operators "Use LoopControl to parallelize AMR operato
 {
 } "no"
 
+BOOLEAN use_openmp "Use OpenMP to parallelize AMR operators" STEERABLE=always
+{
+} "yes"
+
 
 
 restricted:

CarpetLib/src/copy_3d.cc
@@ -30,6 +30,8 @@ void copy_3d(T const *restrict const src, ivect3 const &restrict srcpadext,
              ibbox3 const &restrict srcbbox, ibbox3 const &restrict dstbbox,
              ibbox3 const &restrict srcregbbox1,
              ibbox3 const &restrict dstregbbox, void *extraargs) {
+  DECLARE_CCTK_PARAMETERS;
+
   islab const *restrict const slabinfo = static_cast<islab const *>(extraargs);
 
   if (any(srcbbox.stride() != dstregbbox.stride() or
@@ -113,13 +115,24 @@ void copy_3d(T const *restrict const src, ivect3 const &restrict srcpadext,
   ptrdiff_t const dstjoff = dstoff[1];
   ptrdiff_t const dstkoff = dstoff[2];
 
-// Loop over region
+  // Loop over region
+  if (use_openmp) {
 #pragma omp parallel for collapse(3)
-  for (int k = 0; k < regkext; ++k) {
-    for (int j = 0; j < regjext; ++j) {
-      for (int i = 0; i < regiext; ++i) {
+    for (int k = 0; k < regkext; ++k) {
+      for (int j = 0; j < regjext; ++j) {
+        for (int i = 0; i < regiext; ++i) {
 
-        dst[DSTIND3(i, j, k)] = src[SRCIND3(i, j, k)];
+          dst[DSTIND3(i, j, k)] = src[SRCIND3(i, j, k)];
+        }
+      }
+    }
+  } else {
+    for (int k = 0; k < regkext; ++k) {
+      for (int j = 0; j < regjext; ++j) {
+        for (int i = 0; i < regiext; ++i) {
+
+          dst[DSTIND3(i, j, k)] = src[SRCIND3(i, j, k)];
+        }
       }
     }
   }

CarpetLib/src/copy_4d.cc
@@ -30,6 +30,8 @@ void copy_4d(T const *restrict const src, ivect4 const &restrict srcpadext,
              ibbox4 const &restrict srcbbox, ibbox4 const &restrict dstbbox,
              ibbox4 const &restrict, ibbox4 const &restrict regbbox,
              void *extraargs) {
+  DECLARE_CCTK_PARAMETERS;
+
   assert(not extraargs);
 
   if (any(srcbbox.stride() != regbbox.stride() or
@@ -98,14 +100,27 @@ void copy_4d(T const *restrict const src, ivect4 const &restrict srcpadext,
   ptrdiff_t const dstkoff = dstoff[2];
   ptrdiff_t const dstloff = dstoff[3];
 
-// Loop over region
+  // Loop over region
+  if (use_openmp) {
 #pragma omp parallel for collapse(4)
-  for (int l = 0; l < reglext; ++l) {
-    for (int k = 0; k < regkext; ++k) {
-      for (int j = 0; j < regjext; ++j) {
-        for (int i = 0; i < regiext; ++i) {
+    for (int l = 0; l < reglext; ++l) {
+      for (int k = 0; k < regkext; ++k) {
+        for (int j = 0; j < regjext; ++j) {
+          for (int i = 0; i < regiext; ++i) {
 
-          dst[DSTIND4(i, j, k, l)] = src[SRCIND4(i, j, k, l)];
+            dst[DSTIND4(i, j, k, l)] = src[SRCIND4(i, j, k, l)];
+          }
+        }
+      }
+    }
+  } else {
+    for (int l = 0; l < reglext; ++l) {
+      for (int k = 0; k < regkext; ++k) {
+        for (int j = 0; j < regjext; ++j) {
+          for (int i = 0; i < regiext; ++i) {
+
+            dst[DSTIND4(i, j, k, l)] = src[SRCIND4(i, j, k, l)];
+          }
         }
       }
     }

commit a67f53d58431656cc0bb3d7d24d7ef9cccf24153

CarpetLib: Fix compile error
This was introduced in fe8d0d5b91c4ebb6d63971e356d2e3926e65030d, which
removed several preprocessor conditionals but appears to have missed
this one.

CarpetLib/src/data.cc
@@ -96,7 +96,6 @@ call_operator(void (*the_operator)(
     }
     assert(alldstregbboxes == ibset(dstregbbox));
 #endif
-#endif
   }
 }
 