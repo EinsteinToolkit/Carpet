8d10dd8c780cd86236a5a62f445be6dc3a8e5fa1
Erik Schnetter <schnetter@gmail.com>
2016-11-02 18:36:51 -0400

    CarpetLib: Add some missing include guards

--- CarpetLib/src/limits.hh
+++ CarpetLib/src/limits.hh
@@ -1,3 +1,6 @@
+#ifndef LIMITS_HH
+#define LIMITS_HH
+
 #include <cctk.h>
 
 namespace CarpetLib {
@@ -5,3 +8,5 @@ namespace CarpetLib {
 void set_system_limits();
 
 } // namespace CarpetLib
+
+#endif // #ifndef LIMITS_HH
