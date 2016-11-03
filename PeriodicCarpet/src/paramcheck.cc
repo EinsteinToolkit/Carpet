4b77403b8f3dbc85498daf5803d764c553b5176e
Erik Schnetter <schnetter@gmail.com>
2016-11-02 20:38:03 -0400

    PeriodicCarpet: Source code cleanup

--- PeriodicCarpet/src/paramcheck.cc
+++ PeriodicCarpet/src/paramcheck.cc
@@ -6,9 +6,8 @@ extern "C" void PeriodicCarpet_ParamCheck(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
-  if (periodic and (periodic_x or periodic_y or periodic_z)) {
+  if (periodic and (periodic_x or periodic_y or periodic_z))
     CCTK_PARAMWARN("When PeriodicCarpet::periodic is set, all boundaries are "
                    "periodic. It does not make sense to also set any of "
                    "PeriodicCarpet::periodic_[xyz]");
-  }
 }
