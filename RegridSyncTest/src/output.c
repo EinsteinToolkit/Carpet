490325335a0cb8c076ea30f89573fafa633deb48
Samuel Cupp <scupp1@my.apsu.edu>
2018-03-13 13:28:33 -0500

    Updated RegridSyncTest to use PreSync

--- RegridSyncTest/src/output.c
+++ RegridSyncTest/src/output.c
@@ -1,11 +1,12 @@
 #include <cctk.h>
 #include <cctk_Parameters.h>
 #include <cctk_Arguments.h>
+#include <cctk_Arguments_RegridSyncTest.h>
 
-void regridsynctest_output(CCTK_ARGUMENTS) {
+void RegridSyncTest_output(CCTK_ARGUMENTS) {
 
   DECLARE_CCTK_PARAMETERS;
-  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_ARGUMENTS_RegridSyncTest_output;
 
   CCTK_OutputVarAsByMethod(cctkGH, "regridsynctest::myregridtestgf",
                            "IOASCII_1D", "myregridtestgf_postregrid");
--- RegridSyncTest/src/regridsynctest.F90
+++ RegridSyncTest/src/regridsynctest.F90
@@ -1,25 +1,30 @@
 #include <cctk.h>
 #include <cctk_Parameters.h>
 #include <cctk_Arguments.h>
+#include <cctk_Arguments_RegridSyncTest.h>
 #include <cctk_Functions.h>
 
 
 
-subroutine RegridSyncTest_setup(CCTK_ARGUMENTS)
+subroutine RegridSyncTest_setup(CCTK_ARGUMENTS_RegridSyncTest_setup)
+!subroutine RegridSyncTest_setup(CCTK_ARGUMENTS)
 
   implicit none
   DECLARE_CCTK_PARAMETERS
-  DECLARE_CCTK_ARGUMENTS
+  DECLARE_CCTK_ARGUMENTS_RegridSyncTest_setup
+!  DECLARE_CCTK_ARGUMENTS
 
   myregridtestgf = 0.9d0
 
 end subroutine RegridSyncTest_setup
 
-subroutine RegridSyncTest_sync(CCTK_ARGUMENTS)
+subroutine RegridSyncTest_sync(CCTK_ARGUMENTS_RegridSyncTest_sync)
+!subroutine RegridSyncTest_sync(CCTK_ARGUMENTS)
 
   implicit none
   DECLARE_CCTK_PARAMETERS
-  DECLARE_CCTK_ARGUMENTS
+  DECLARE_CCTK_ARGUMENTS_RegridSyncTest_sync
+!  DECLARE_CCTK_ARGUMENTS
 
 !  :-)
 
@@ -27,11 +32,13 @@ end subroutine RegridSyncTest_sync
 
 
 
-subroutine RegridSyncTest_do_something(CCTK_ARGUMENTS)
+subroutine RegridSyncTest_do_something(CCTK_ARGUMENTS_RegridSyncTest_do_something)
+!subroutine RegridSyncTest_do_something(CCTK_ARGUMENTS)
 
   implicit none
   DECLARE_CCTK_PARAMETERS
-  DECLARE_CCTK_ARGUMENTS
+  DECLARE_CCTK_ARGUMENTS_RegridSyncTest_do_something
+!  DECLARE_CCTK_ARGUMENTS
 
   myregridtestgf = 1.0d0
 
@@ -39,11 +46,13 @@ end subroutine RegridSyncTest_do_something
 
 
 
-subroutine RegridSyncTest_evolve(CCTK_ARGUMENTS)
+subroutine RegridSyncTest_evolve(CCTK_ARGUMENTS_RegridSyncTest_evolve)
+!subroutine RegridSyncTest_evolve(CCTK_ARGUMENTS)
 
   implicit none
   DECLARE_CCTK_PARAMETERS
-  DECLARE_CCTK_ARGUMENTS
+  DECLARE_CCTK_ARGUMENTS_RegridSyncTest_evolve
+!  DECLARE_CCTK_ARGUMENTS
 
   myregridtestgf = 0.8d0
 
