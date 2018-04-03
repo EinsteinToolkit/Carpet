f19cc1aab2f52144a8a9a4237c249e89a3eab228
Samuel Cupp <scupp1@my.apsu.edu>
2018-04-03 13:11:34 -0500

    Added error messages for sync call in IOASCII

--- CarpetIOASCII/src/ioascii.cc
+++ CarpetIOASCII/src/ioascii.cc
@@ -494,12 +494,14 @@ void IOASCII<outdim>::OutputDirection(const cGH *const cctkGH, const int vindex,
     assert(not ierr);
   }
 
-  if(output_boundary_points) {
-    int valid = Carpet_GetValidRegion(vindex,0);
-    if(valid == WH_INTERIOR) {
-      Carpet_ManualSyncGF(cctkGH,vindex);
-      Carpet_SetValidRegion(vindex, 0,WH_EVERYWHERE);
-    }
+  int valid = Carpet_GetValidRegion(vindex,0);
+  if(valid == WH_INTERIOR) {
+    std::cout << "ASCII Syncing " << CCTK_FullName(vindex) << std::endl;
+    Carpet_ManualSyncGF(cctkGH,vindex);
+  } else if(valid == WH_NOWHERE || valid == WH_BOUNDARY || valid == WH_GHOSTS) {
+    std::string vname = CCTK_FullName(vindex);
+    std::string msg = "Attempted ASCII output of the variable " + vname + " failed because the interior was invalid";
+    CCTK_ERROR(msg.c_str());
   }
 
   const int ml = groupdata.grouptype == CCTK_GF ? mglevel : 0;
