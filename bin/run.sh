917b351909873b450e890e837710eb97d63f55ab
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-02-13 11:10:32 -0600

    Add debug tools

---
+++ bin/run.sh
@@ -0,0 +1,9 @@
+set -e
+export RDWR_DEBUG_VARS="ML_BSSN::gt11 ADMBASE::gxx ML_BSSN::phi"
+export RDWR_DEBUG_INDEXES="1 1 1"
+./exe/cactus_sim -Roe yes.par
+mv CCTK_Proc0.out CCTK_Proc0.yes
+./exe/cactus_sim -Roe no.par
+mv CCTK_Proc0.out CCTK_Proc0.no
+python3 trim.py
+vimdiff x.yes x.no
