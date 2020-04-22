9577b3d10a9cc204ba21337d1e9810a29479a722
Roland Haas <rhaas@ncsa.illinois.edu>
2020-04-22 16:26:03 -0500

    CarpetProlongateTest: simplify code concepts used

--- CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
+++ CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
@@ -1,18 +1,16 @@
 #include <cctk.h>
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
-#include <string>
-#include <vector>
 #include <assert.h>
 
 void CarpetProlongateSelectBCs(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
-  std::vector<std::string> v{"CARPETPROLONGATETEST::u",
-                             "CARPETPROLONGATETEST::u0",
-                             "CARPETPROLONGATETEST::du"};
-  for (auto var : v) {
-    int ierr = Boundary_SelectVarForBC(
-        cctkGH, CCTK_ALL_FACES, cctk_nghostzones[0], -1, var.c_str(), "None");
+  char const *const varnames[] = {"CARPETPROLONGATETEST::u",
+                                  "CARPETPROLONGATETEST::u0",
+                                  "CARPETPROLONGATETEST::du"};
+  for (auto var : varnames) {
+    int ierr = Boundary_SelectVarForBC(cctkGH, CCTK_ALL_FACES,
+                                       cctk_nghostzones[0], -1, var, "None");
     assert(ierr != -1);
   }
 }
