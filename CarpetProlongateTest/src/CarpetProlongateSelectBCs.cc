b6c25943c22053870c9212d54ac1eb076d3f0798
Roland Haas <rhaas@ncsa.illinois.edu>
2020-04-22 16:41:56 -0500

    CarpetProlongateTest: use CCTK_VERROR to report errors

--- CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
+++ CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
@@ -1,17 +1,17 @@
 #include <cctk.h>
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
-#include <assert.h>
 
-extern "C"
-void CarpetProlongateTest_SelectBCs(CCTK_ARGUMENTS) {
+extern "C" void CarpetProlongateTest_SelectBCs(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
   char const *const varnames[] = {"CARPETPROLONGATETEST::u",
                                   "CARPETPROLONGATETEST::u0",
                                   "CARPETPROLONGATETEST::du"};
   for (auto var : varnames) {
-    int ierr = Boundary_SelectVarForBC(cctkGH, CCTK_ALL_FACES,
-                                       cctk_nghostzones[0], -1, var, "None");
-    assert(ierr != -1);
+    int const ierr = Boundary_SelectVarForBC(
+        cctkGH, CCTK_ALL_FACES, cctk_nghostzones[0], -1, var, "None");
+    if (ierr != 0) {
+      CCTK_VERROR("Boundary_SelectVarForBC failed: %d", ierr);
+    }
   }
 }
