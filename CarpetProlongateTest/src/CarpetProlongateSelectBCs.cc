50645e1b7867d6c65f2532d172c5414e9dae622e
Roland Haas <rhaas@ncsa.illinois.edu>
2020-04-22 16:23:46 -0500

    CarpetProlongateTest: reformat C++ source code
    no other change

--- CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
+++ CarpetProlongateTest/src/CarpetProlongateSelectBCs.cc
@@ -5,18 +5,14 @@
 #include <vector>
 #include <assert.h>
 
-void CarpetProlongateSelectBCs(CCTK_ARGUMENTS)
-{
+void CarpetProlongateSelectBCs(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
-  std::vector<std::string> v{
-    "CARPETPROLONGATETEST::u",
-    "CARPETPROLONGATETEST::u0",
-    "CARPETPROLONGATETEST::du"
-  };
-  for(auto var : v) {
+  std::vector<std::string> v{"CARPETPROLONGATETEST::u",
+                             "CARPETPROLONGATETEST::u0",
+                             "CARPETPROLONGATETEST::du"};
+  for (auto var : v) {
     int ierr = Boundary_SelectVarForBC(
-      cctkGH, CCTK_ALL_FACES, cctk_nghostzones[0],
-      -1, var.c_str(), "None");
+        cctkGH, CCTK_ALL_FACES, cctk_nghostzones[0], -1, var.c_str(), "None");
     assert(ierr != -1);
   }
 }
